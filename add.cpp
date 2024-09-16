da2e39274f3fae1a83b47782be281a0f47689bb6
pavan77k <166364643+pavan77k@users.noreply.github.com>
2024-09-17 00:05:08 +0530

    Add files via upload

---
+++ add.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "add.h"
+
+int main()
+{
+	a = 10;
+	b = 20;
+
+	int result = 0;
+
+	result = sum(a, b);
+
+	printf("Sum = %d\n", result);
+}
---
+++ add.h
@@ -0,0 +1,9 @@
+#pragma once
+int a;
+int b;
+
+
+int sum(int a, int b)
+{
+	return (a + b);
+}
